36814efe0c3779ffe34c69f3e19d548a0be53704
Nelson Cadenas <nca1478@yahoo.com>
2021-12-06 22:42:56 -0400

    added: flow and ui for user registration

--- src/components/pages/Users/Register.js
+++ src/components/pages/Users/Register.js
@@ -1,102 +1,157 @@
-import { Link } from 'react-router-dom'
+// Dependencies
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify'
+
+// Api
+import { post } from '../../../config/api'
+
+// Context
+import { AuthContext } from '../../../auth/authContext'
+
+// Types Reducer
+import { types } from '../../../types/types'
 
 export const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const { dispatch } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const onSubmit = (data) => {
+    // post('/users/login', data)
+    //   .then((response) => {
+    //     if (response.data === null) {
+    //       toast.error(response.errors.msg)
+    //     } else {
+    //       const dataUser = {
+    //         token: response.data.token,
+    //         ...response.data.user,
+    //       }
+    //       dispatch({
+    //         type: types.login,
+    //         payload: { data: dataUser },
+    //       })
+    //       navigate('/dashboard', { replace: true })
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     toast.error('Please verify the data entered and try again.')
+    //     console.log(error)
+    //   })
+  }
+
   return (
-    <section class="p-4 bg-light">
-      <div class="container" style={{ width: '420px' }}>
-        <div class="row text-center">
-          <div class="col-md">
-            <div class="card text-dark py-3">
-              <div class="card-body text-center">
-                <div class="h1 mb-3">
-                  <i class="bi bi-person-circle"></i>
-                </div>
-                <h3 class="card-title mb-3">Create an Account</h3>
+    <Container className="my-4" style={{ width: '420px' }}>
+      <Row className="text-center">
+        <Col>
+          <Card className="text-dark py-3">
+            <Card.Body className="text-center">
+              <h1 className="mb-3">
+                <i className="bi bi-person-circle"></i>
+              </h1>
+              <h3 className="card-title mb-3">Create an Account</h3>
 
-                <form class="row needs-validation mx-1" novalidate>
-                  {/* Fullname */}
-                  <div class="input-group has-validation mb-3">
-                    <span class="input-group-text" id="inputGroupPrepend">
-                      <i class="bi bi-person-circle"></i>
-                    </span>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="name"
-                      aria-describedby="inputGroupPrepend"
-                      placeholder="Fullname"
-                      required
-                    />
-                    <div class="invalid-feedback">Enter your username.</div>
-                  </div>
-
-                  {/* Email */}
-                  <div class="input-group has-validation mb-3">
-                    <span class="input-group-text" id="inputGroupPrepend">
-                      <i class="bi bi-envelope"></i>
-                    </span>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="email"
-                      aria-describedby="inputGroupPrepend"
-                      placeholder="Email"
-                      required
-                    />
-                    <div class="invalid-feedback">Enter your username.</div>
-                  </div>
-
-                  {/* Password */}
-                  <div class="input-group has-validation mb-3">
-                    <span class="input-group-text" id="inputGroupPrepend2">
-                      <i class="bi bi-key"></i>
-                    </span>
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="password"
-                      aria-describedby="inputGroupPrepend2"
-                      placeholder="Password"
-                      required
-                    />
-                    <div class="invalid-feedback">Enter your password.</div>
-                  </div>
-
-                  {/* Confirm Password */}
-                  <div class="input-group has-validation mb-3">
-                    <span class="input-group-text" id="inputGroupPrepend2">
-                      <i class="bi bi-key"></i>
-                    </span>
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="confirm"
-                      aria-describedby="inputGroupPrepend2"
-                      placeholder="Confirm Password"
-                      required
-                    />
-                    <div class="invalid-feedback">Enter your password.</div>
-                  </div>
-
-                  {/* Register Button */}
-                  <div class="d-grid gap-2 mb-3">
-                    <button class="btn btn-primary" type="submit">
-                      Register
-                    </button>
-                  </div>
-
-                  <span class="card-title">
-                    Do you already have an account?{' '}
-                    <Link to="/login" style={{ textDecoration: 'none' }}>
-                      Login
-                    </Link>
+              <Form className="mx-3" onSubmit={handleSubmit(onSubmit)}>
+                {/* Fullname */}
+                <InputGroup className="mb-3">
+                  <span className="input-group-text">
+                    <i className="bi bi-person-circle"></i>
                   </span>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+                  <FormControl
+                    placeholder="Fullname"
+                    {...register('name', { required: true })}
+                    autoComplete="off"
+                  />
+                  {errors.name && (
+                    <Form.Text className="text-danger w-100">
+                      Fullname is required
+                    </Form.Text>
+                  )}
+                </InputGroup>
+
+                {/* Email */}
+                <InputGroup className="mb-3">
+                  <span className="input-group-text">
+                    <i className="bi bi bi-envelope"></i>
+                  </span>
+                  <FormControl
+                    placeholder="Email"
+                    {...register('email', { required: true })}
+                    autoComplete="off"
+                  />
+                  {errors.email && (
+                    <Form.Text className="text-danger w-100">
+                      Email is required
+                    </Form.Text>
+                  )}
+                </InputGroup>
+
+                {/* Password */}
+                <InputGroup className="mb-3">
+                  <span className="input-group-text">
+                    <i className="bi bi-key"></i>
+                  </span>
+                  <FormControl
+                    type="password"
+                    placeholder="Password"
+                    {...register('password', { required: true })}
+                  />
+                  {errors.password && (
+                    <Form.Text className="text-danger w-100">
+                      Password is required
+                    </Form.Text>
+                  )}
+                </InputGroup>
+
+                {/* Confirm Password */}
+                <InputGroup className="mb-3">
+                  <span className="input-group-text">
+                    <i className="bi bi-key"></i>
+                  </span>
+                  <FormControl
+                    type="confirm"
+                    placeholder="Confirm Password"
+                    {...register('confirm', { required: true })}
+                  />
+                  {errors.confirm && (
+                    <Form.Text className="text-danger w-100">
+                      Password is required
+                    </Form.Text>
+                  )}
+                </InputGroup>
+
+                {/* Register Button */}
+                <Button type="submit" variant="primary" className="w-100 mb-3">
+                  Register
+                </Button>
+
+                <span className="card-title">
+                  Do you already have an account?{' '}
+                  <Link to="/login" style={{ textDecoration: 'none' }}>
+                    Login
+                  </Link>
+                </span>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <ToastContainer />
+    </Container>
   )
 }
