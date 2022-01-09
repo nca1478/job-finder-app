ea39b9cb2c809774722560dc26d71eee9731c59a
Nelson Cadenas <nca1478@yahoo.com>
2022-01-08 21:01:43 -0400

    added: google authentication on login page

--- src/components/pages/Users/Login.js
+++ src/components/pages/Users/Login.js
@@ -4,6 +4,8 @@ import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap'
 import { ToastContainer, toast } from 'react-toastify'
+import GoogleLogin from 'react-google-login'
+import FacebookLogin from 'react-facebook-login'
 
 // Fetch Config
 import { post } from '../../../config/api'
@@ -44,9 +46,65 @@ export const LoginPage = () => {
         }
       })
       .catch((error) => {
-        toast.error('Please verify the data entered and try again.')
+        toast.error('Error when trying to login.')
         console.log(error)
       })
+  }
+
+  const responseGoogle = (response) => {
+    const tokenId = { tokenId: response.tokenId }
+    post('/users/google', tokenId)
+      .then((response) => {
+        if (response.data === null) {
+          toast.error(response.errors.msg)
+        } else {
+          const dataUser = {
+            token: response.data.token,
+            ...response.data.user,
+          }
+          dispatch({
+            type: types.login,
+            payload: { data: dataUser },
+          })
+          navigate('/dashboard', { replace: true })
+        }
+      })
+      .catch((error) => {
+        toast.error('Error when trying to google login.')
+        console.log(error)
+      })
+  }
+
+  const responseFacebook = (response) => {
+    const facebookData = {
+      accessToken: response.accessToken,
+      userID: response.userID,
+    }
+    console.log(facebookData)
+    // post('/users/facebook', facebookData)
+    //     .then((response) => {
+    //         if (response.data === null) {
+    //             notification['error']({
+    //                 message: 'Login Error',
+    //                 description: 'An error has occurred starting the facebook session.',
+    //             });
+    //         } else {
+    //             const dataUser = {
+    //                 token: response.data.token,
+    //                 ...response.data.user,
+    //             };
+    //             authenticate(dataUser, () => {
+    //                 history.replace(from);
+    //             });
+    //         }
+    //     })
+    //     .catch((error) => {
+    //         notification['error']({
+    //             message: 'Login Error',
+    //             description: 'An error has occurred starting the facebook session.',
+    //         });
+    //         console.log(error);
+    //     });
   }
 
   return (
@@ -91,17 +149,32 @@ export const LoginPage = () => {
 
                   <h5 className="card-title my-3">Or</h5>
 
-                  <Button type="button" variant="danger" className="w-100 mb-3">
-                    <i className="bi bi-google"></i> Login with Google
-                  </Button>
+                  <GoogleLogin
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    render={(renderProps) => (
+                      <Button
+                        type="button"
+                        variant="danger"
+                        className="w-100"
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                      >
+                        <i className="bi bi-google"></i> Login with Google
+                      </Button>
+                    )}
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                  />
 
-                  <Button
-                    type="button"
-                    variant="primary"
-                    className="w-100 mb-3"
-                  >
-                    <i className="bi bi-facebook"></i> Login with Facebook
-                  </Button>
+                  <FacebookLogin
+                    appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+                    callback={responseFacebook}
+                    cssClass="button-submit facebook-button"
+                    textButton={<span>Login with Facebook</span>}
+                    icon="fa-facebook"
+                  />
 
                   <span className="card-title">
                     Do you need an account?{' '}
