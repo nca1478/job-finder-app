1c357d2e39bde298d9e826b743c0b6565fdc57f8
Nelson Cadenas <nca1478@yahoo.com>
2022-05-11 21:26:21 -0400

    updated: copyright footer section (Home Page)

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -5,7 +5,19 @@ export const Footer = () => {
   return (
     <div className="p-3 bg-dark text-white text-center">
       <Container>
-        <p className="lead pt-3">Copyright &copy; 2022 Job Finder App</p>
+        <p className="lead pt-3">
+          Copyright &copy; 2022
+          <br />
+          Developed by:{' '}
+          <a
+            href="https://nelsoncadenas.netlify.app/index.html"
+            rel="noreferrer"
+            target="_blank"
+            className="text-decoration-none fw-bold"
+          >
+            Nelson Cadenas
+          </a>
+        </p>
       </Container>
     </div>
   )
